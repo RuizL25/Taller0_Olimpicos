1b62dd19ae899007152714f00e2f08356dad2135
RuizL25 <99106138+RuizL25@users.noreply.github.com>
2023-01-29 17:37:30 -0500

    Taller1
    Taller 1 modificaciones

---
+++ src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -0,0 +1,27 @@
+package uniandes.dpoo.taller0.modificacion;
+import java.io.IOException;
+
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+
+public class Modificacion {
+
+	public Modificacion() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hola, mundo!");
+		CalculadoraEstadisticas calc;
+		try {
+			calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+			System.out.println(calc.paisConMasMedallistas());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
